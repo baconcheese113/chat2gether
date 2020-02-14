bc51b110acc7d3cf86b4a8499fc3069158a403ba
Joseph Yannessa <baconcheese113@hotmail.com>
2020-02-13 23:40:01 -0700

    More praying to the heroku gods

--- server/src/utils/hashPassword.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcryptjs'
-
-export default (password) => {
-  if(password.length < 8) {
-    throw new Error('Password must be 8 characters or longer.')
-  }
-  return bcrypt.hash(password, 10)
-}
