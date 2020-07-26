7417c51c4b76cafa687f37ea79d146180b0f1a90
Joseph Yannessa <baconcheese113@hotmail.com>
2020-07-25 19:26:05 -0600

    [FE] Fix resize issues on mobile

--- client/src/hooks/WindowSizeHook.js
+++ client/src/hooks/WindowSizeHook.js
@@ -1,16 +1,16 @@
 import React from 'react'
 
 export default function useWindowSize() {
-  const [windowSize, setWindowSize] = React.useState({ innerWidth: window.innerWidth, innerHeight: window.innerHeight })
-  const { innerHeight, innerWidth } = windowSize
+  const [innerHeight, setInnerHeight] = React.useState(window.innerHeight)
+  const [innerWidth, setInnerWidth] = React.useState(window.innerWidth)
 
   const isPC = React.useMemo(() => innerWidth > 600, [innerWidth])
   const flowDirection = React.useMemo(() => (innerWidth > innerHeight ? 'row' : 'column'), [innerHeight, innerWidth])
 
   const updateSize = React.useCallback(() => {
-    if (innerWidth === window.innerWidth && innerHeight === window.innerHeight) return
-    setWindowSize({ innerWidth: window.innerWidth, innerHeight: window.innerHeight })
-  }, [innerHeight, innerWidth])
+    setInnerHeight(window.innerHeight)
+    setInnerWidth(window.innerWidth)
+  }, [])
 
   React.useEffect(() => {
     window.addEventListener('resize', updateSize)
